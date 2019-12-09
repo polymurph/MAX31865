77314856d10125ff1ec1e4ae58aba7093484dffb
polymurph <eddyed.k@gmail.com>
2019-12-09 15:58:00 +0100

    added basic API structure

--- HWO/max31865.h
+++ HWO/max31865.h
@@ -9,6 +9,48 @@
 #ifndef MAX31865_H_
 #define MAX31865_H_
 #include <stdint.h>
+#include <stdbool.h>
+
+typedef void (*fptr_t)(void)
+typedef void (*fptr_b_t)(bool);
+typedef uint8_t (*u8_fptr_u8_t)(uint8_t);
+
+typedef struct{
+    fptr_b_t chipselect;
+    u8_fptr_u8_t spi_trx;
+    fptr_t charged_time_delay;
+    fptr_t conversion_timer_deay;
+    uint16_t rtd;
+    uint16_t rref;
+    uint16_t lowFaultThreshold;
+    uint16_t highFaultThreshold;
+    uint8_t configReg;
+}max31865_t;
+
+
+uint16_t max31865_init(max31865_t*  device,
+                       uint16_t     rtd_ohm,
+                       uint16_t     rref_ohm,
+                       uint16_t     lowerFaulThreshold,
+                       uint16_t     higherFaultThreshold);
+
+uint16_t max31865_readADC(max31865_t* device);
+
+float max31865_readRTD_ohm(const max31865_t* device);
+
+float max31865_readCelsius(const max31865_t* device);
+
+float max31865_readKelvin(const max31865_t* device);
+
+void max31865_setHighFaultThreshold(max31865_t* device, uint16_t threshold);
+
+void max31865_setLowFaultThreshold(max31865_t* device, uint16_t threshold);
+
+int8_t max31865_checkThresholdFault(const max31865_t* device);
+
+void max31865_clearFault(const max31865_t* device);
+
+#if 0
 
 #define NULL_PTR (0)
 #define RTD_CAPACITOR_CHARGETIME_ms (2)
@@ -56,5 +98,6 @@ void max31865_setLowFaultThreshold(max31865_t device, uint16_t threshold);
 int8_t max31865_checkThresholdFault(max31865_t device);
 
 void max31865_clearFault(max31865_t device);
+#endif
 
 #endif /* MAX31865_H_ */
